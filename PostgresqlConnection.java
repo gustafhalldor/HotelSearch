7a2c0ceb1b9fa323dc6d738ddcbe06ca8c9c45ef
gustafhalldor <gustafh@gmail.com>
2016-03-26 21:07:59 +0000

    trying to create a connection

--- PostgresqlConnection.java
+++ PostgresqlConnection.java
@@ -3,9 +3,14 @@ import java.sql.*;
 
 public class PostgresqlConnection {
 
-	Connection conn = null;
-	
-//	public static Connection Connector() {
-//		
-//	}
+	public static void main(String[] args){
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/HotelSearch", 
+					"postgres", "gusti123");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
