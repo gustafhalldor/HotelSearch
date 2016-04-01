93cd4ea034001efe67b8c9757110ea1af19074e7
annalobers <akl4@hi.is>
2016-04-01 13:16:33 +0000

    anna er best

--- Controller.java
+++ Controller.java
@@ -39,64 +39,70 @@ public class Controller {
 	// Þá er checkin dagsetningin *****403 vissulega á milli *****331 og *****401 og herbergið þá ekki í hópi þeirra sem eru laus.
 	// Þetta er ekkert spes lausn, en virkar fyrir svona lítið prógramm eins og við erum með. Ef herbergin væru fleiri, þá væri þetta glatað.
 	
-	public static Room [] getAvailability(){
+//	public static Room [] getAvailability(long checkIn, long checkOut, int nrOfRooms){
+	
+	public boolean getAvailability(long checkIn, long checkOut, int nrOfRooms){
+		ControllerMockTrue mocker = new ControllerMockTrue();
+		return mocker.getAvailability(checkIn, checkOut, nrOfRooms);
+
+		
 	
 	
-		int rand = (int) (Math.random()*5);
-		if (rand == 0){
-			Room[] availableRooms = new Room[3];
-
-			availableRooms[0] = new Room(1, 1, "Double", 90);
-			availableRooms[1] = new Room(5, 2, "Double", 100);
-			availableRooms[2] = new Room(9, 3, "Double", 110);
-			
-			return availableRooms;
-		}
-		
-		else if(rand == 1){
-			Room[] availableRooms = new Room[3];
-
-			availableRooms[0] = new Room(4, 2, "Double", 90);
-			availableRooms[1] = new Room(8, 3, "Double", 100);
-			availableRooms[2] = new Room(12, 4, "Double", 110);
-			
-			return availableRooms;
-		}
-		
-		else if (rand == 2){
-			Room[] availableRooms = new Room[3];
-
-			availableRooms[0] = new Room(7, 3, "Double", 90);
-			availableRooms[1] = new Room(11, 4, "Double", 100);
-			availableRooms[2] = new Room(15, 5, "Double", 110);
-			
-			return availableRooms;
-		}
-		
-		else if (rand == 3){
-			Room[] availableRooms = new Room[3];
-
-			availableRooms[0] = new Room(10, 4, "Double", 90);
-			availableRooms[1] = new Room(14, 5, "Double", 100);
-			availableRooms[2] = new Room(3, 1, "Double", 110);
-			
-			return availableRooms;
-		}
-		
-		else {
-			Room[] availableRooms = new Room[3];
-
-			availableRooms[0] = new Room(13, 5, "Double", 90);
-			availableRooms[1] = new Room(2, 1, "Double", 100);
-			availableRooms[2] = new Room(6, 2, "Double", 110);
-			
-			return availableRooms;
-		}
+//		int rand = (int) (Math.random()*5);
+//		if (rand == 0){
+//			Room[] availableRooms = new Room[3];
+//
+//			availableRooms[0] = new Room(1, 1, "Double", 90);
+//			availableRooms[1] = new Room(5, 2, "Double", 100);
+//			availableRooms[2] = new Room(9, 3, "Double", 110);
+//			
+//			return availableRooms;
+//		}
+//		
+//		else if(rand == 1){
+//			Room[] availableRooms = new Room[3];
+//
+//			availableRooms[0] = new Room(4, 2, "Double", 90);
+//			availableRooms[1] = new Room(8, 3, "Double", 100);
+//			availableRooms[2] = new Room(12, 4, "Double", 110);
+//			
+//			return availableRooms;
+//		}
+//		
+//		else if (rand == 2){
+//			Room[] availableRooms = new Room[3];
+//
+//			availableRooms[0] = new Room(7, 3, "Double", 90);
+//			availableRooms[1] = new Room(11, 4, "Double", 100);
+//			availableRooms[2] = new Room(15, 5, "Double", 110);
+//			
+//			return availableRooms;
+//		}
+//		
+//		else if (rand == 3){
+//			Room[] availableRooms = new Room[3];
+//
+//			availableRooms[0] = new Room(10, 4, "Double", 90);
+//			availableRooms[1] = new Room(14, 5, "Double", 100);
+//			availableRooms[2] = new Room(3, 1, "Double", 110);
+//			
+//			return availableRooms;
+//		}
+//		
+//		else {
+//			Room[] availableRooms = new Room[3];
+//
+//			availableRooms[0] = new Room(13, 5, "Double", 90);
+//			availableRooms[1] = new Room(2, 1, "Double", 100);
+//			availableRooms[2] = new Room(6, 2, "Double", 110);
+//			
+//			return availableRooms;
+//		}
 
 	}
 	
 	// dates are of the format 20160327, which means year 2016, month 03 and day 27.
-	public static int setAvailability(int roomid, int guestid, long checkin, long checkout) throws SQLException{
+	public int setAvailability(int roomid, int guestid, long checkin, long checkout) throws SQLException{
 		/*Connection conn = PostgresqlConnection.getConnection();
 		Statement stmt = conn.createStatement();
 		String sql = "insert into occupied_room values("+roomid+","+guestid+","+checkin+","+checkout+")";
